5925931bad740037d8419121989e8e02d2276582
adigitalnative@gmail.com <adigitalnative@gmail.com>
2018-08-23 15:00:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObject = object
+  var newObject = Object.assign({}, object)
   delete newObject.key
   
   return newObject
