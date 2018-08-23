39526cbef520411bc1a61a3a055a437153241730
adigitalnative@gmail.com <adigitalnative@gmail.com>
2018-08-23 14:56:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]:value })
 }
-
-// function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-//   return object[key] = value
-// }
